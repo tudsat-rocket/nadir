4e293a711fd6ebf41bf624cdc85d6500f3d4aef0
Felix Wiegand <felix-wiegand@protonmail.ch>
2025-12-26 02:11:04 +0100

    Use COPTER_MODE enum for ArduCopter mode info

--- core/src/protocols/modes.rs
+++ core/src/protocols/modes.rs
@@ -1,12 +1,11 @@
 use std::time::Duration;
 
 use maviola::protocol::ComponentId;
-use mavspec::rust::dialects::{
-    Common,
-    common::{
-        enums::MavCmd,
-        messages::{AvailableModes, CommandLong},
-    },
+use mavspec::rust::dialects::Common;
+use mavspec::rust::dialects::ardupilotmega::enums::CopterMode;
+use mavspec::rust::dialects::common::{
+    enums::{MavAutopilot, MavCmd, MavModeProperty, MavStandardMode, MavType},
+    messages::{AvailableModes, CommandLong, Heartbeat},
 };
 use tokio::time::timeout;
 
@@ -17,6 +16,40 @@ pub async fn discover_available_modes(
     component_id: ComponentId,
     mut message_rx: tokio::sync::broadcast::Receiver<Common>,
 ) {
+    let heartbeat = loop {
+        if let Ok(heartbeat) = system.last_message::<Heartbeat>() {
+            break heartbeat;
+        }
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
+    };
+
+    // Arducopter does not support this protocol for some reason
+    if heartbeat.autopilot == MavAutopilot::Ardupilotmega && heartbeat.type_ != MavType::FixedWing {
+        let modes = CopterMode::entries()
+            .enumerate()
+            .map(|(i, cm)| {
+                let mode_name_str = format!("{cm:?}");
+                let mode_name_bytes = mode_name_str.as_bytes();
+
+                let mut mode_name = [0x00; 35];
+                mode_name[..(mode_name_bytes.len())].copy_from_slice(mode_name_bytes);
+
+                AvailableModes {
+                    number_modes: CopterMode::entries().collect::<Vec<_>>().len() as u8,
+                    mode_index: i as u8,
+                    standard_mode: MavStandardMode::NonStandard,
+                    custom_mode: u32::from(cm.value()),
+                    properties: MavModeProperty::empty(),
+                    mode_name,
+                }
+            })
+            .collect();
+
+        *system.available_modes.lock().unwrap() = Some(modes);
+        return;
+    }
+
     tracing::debug!(system_id = system.system_id, "Starting mode discovery.");
 
     loop {
